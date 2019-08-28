f025563aa1418f4556f85815bb746175f6660052
testuser <hardstylelover0604@gmail.com>
2019-08-28 21:11:28 +0900

    0828

--- template/WebContent/buyItemComplete.jsp
+++ template/WebContent/buyItemComplete.jsp
@@ -72,7 +72,7 @@
 		<div>
 			<p>購入手続きが完了致しました。</p>
 			<div>
-				<a href = '<s:url action = "MyPageAcion"/>'>マイページ</a>
+				<a href = '<s:url action = "MyPageAction"/>'>マイページ</a>
 				<span>から購入履歴の確認が可能です。</span>
 			</div>
 		</div>
--- template/sql/ecsite.sql
+++ template/sql/ecsite.sql
@@ -1,9 +1,9 @@
 set names utf8;
 set foreign_key_checks = 0;
-drop database if exists ecsite;
+drop database if exists ecsite_sa;
 
-create database if not exists ecsite;
-use ecsite;
+create database if not exists ecsite_sa;
+use ecsite_sa;
 
 drop table if exists login_user_transaction;
 
